a3069a5e825ba3e60bd7b848f70e12d77622a43c
yomaq <yomaq@bsjm.xyz>
2024-02-19 22:47:37 -0600

    generate hostkeys, and update vmtest

--- hosts/vmtest/default.nix
+++ hosts/vmtest/default.nix
@@ -9,19 +9,17 @@
   config = {
     # hostname
     networking.hostName = "devcontainer";
-    # nixos version on creation
-    system.stateVersion = "23.11";
-    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
     # modules imported above, which are located at ./modules/hosts/
     devcontainer = {
       ssh.enable = true;
-      tailscale.enable = true;
       openvscode-server.enable = true;
       suites = {
         packages.enable = true;
       };
       users.root.enable = true;
+      nixSettings.enable = true;
+      tailscale.enable = true;
     };
   };
 }
--- modules/hosts/ssh/default.nix
+++ modules/hosts/ssh/default.nix
@@ -25,6 +25,10 @@ in
         # PasswordAuthentication = false;
       };
       hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
       ];
     };
   };
